222fb5cfa5fd7b2505f8218d31b02c44217f5453
Dongha Kang <dongha@irisbooth.com>
2021-03-17 23:33:50 -0400

    Set up strapi backend

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8f63cd2fe905e1bf9fb99e4841b726f3'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a891e069-2430-43d6-b471-368d9634af9c'
+};
