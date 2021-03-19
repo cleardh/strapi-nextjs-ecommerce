ca9676a9e6f12d730d9b25762a285c43f950d198
Dongha Kang <dongha@irisbooth.com>
2021-03-19 00:01:44 -0400

    Created next frontend

---
+++ ecommerce-frontend/pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+import products from '../products.json'
+import { fromImageToUrl } from '../utils/urls';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {products.map(product => (
+        <div key={product.name} className={styles.product}>
+          <Link href={`/products/${product.slug}`}>
+            <a>
+              <div className={styles.product__Row}>
+                <div className={styles.product__ColImg}>
+                  <img src={fromImageToUrl(product.image)} />
+                </div>
+                <div className={styles.product__Col}>
+                  {product.name} {product.price}
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+      ))}
+      
+    </div>
+  )
+}
