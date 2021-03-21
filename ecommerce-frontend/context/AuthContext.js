e6f93940a37ef164bd4a606ae86cf13b4b7ca2a4
Dongha Kang <dongha@irisbooth.com>
2021-03-21 15:19:39 -0400

    Tested basic auth process

---
+++ ecommerce-frontend/context/AuthContext.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react'
+import { useRouter } from 'next/router'
+
+const AuthContext = createContext()
+
+export const AuthProvider = (props) => {
+    const [user, setUser] = useState(null)
+    const router = useRouter()
+    /**
+     * Add email to user
+     * @param {string} email 
+     */
+    const loginUser = async (email) => {
+        setUser({ email })
+        router.push('/')
+    }
+    /**
+     * Set user to null for logging out
+     */
+    const logoutUser = async () => {
+        setUser(null)
+        router.push('/')
+    }
+    return (
+        <AuthContext.Provider value={{ user, loginUser, logoutUser }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
