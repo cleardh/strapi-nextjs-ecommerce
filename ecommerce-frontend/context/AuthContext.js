1b67002d2fec99be25871de7411a13e0c8c9a78c
Dongha Kang <dongha@irisbooth.com>
2021-03-21 16:40:14 -0400

    Set up auth with Magic

--- ecommerce-frontend/context/AuthContext.js
+++ ecommerce-frontend/context/AuthContext.js
@@ -1,8 +1,11 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { Magic } from 'magic-sdk'
+import { MAGIC_PUBLIC_KEY } from '../utils/urls'
 
 const AuthContext = createContext()
 
+let magic
 export const AuthProvider = (props) => {
     const [user, setUser] = useState(null)
     const router = useRouter()
@@ -11,16 +14,51 @@ export const AuthProvider = (props) => {
      * @param {string} email 
      */
     const loginUser = async (email) => {
-        setUser({ email })
-        router.push('/')
+        try {
+            await magic.auth.loginWithMagicLink({ email })
+            setUser({ email })
+            router.push('/')
+        } catch (err) {
+            setUser(null)
+        }
     }
     /**
      * Set user to null for logging out
      */
     const logoutUser = async () => {
-        setUser(null)
-        router.push('/')
+        try {
+            await magic.user.logout()
+            setUser(null)
+            router.push('/')
+        } catch (err) {}
     }
+    const checkUserLoggedIn = async () => {
+        try {
+            const isLoggedIn = await magic.user.isLoggedIn()
+            if (isLoggedIn) {
+                const { email } = await magic.user.getMetadata()
+                setUser({ email })
+
+                // For testing
+                const token = await getToken()
+                console.log('Token: ', token)
+            }
+        } catch (err) {}
+    }
+    /**
+     * Retrieves Bearer token Magic issues
+     * Allow user to make requests
+     */
+    const getToken = async () => {
+        try {
+            const token = await magic.user.getIdToken()
+            return token
+        } catch (err) {}
+    }
+    useEffect(() => {
+        magic = new Magic(MAGIC_PUBLIC_KEY)
+        checkUserLoggedIn()
+    }, [])
     return (
         <AuthContext.Provider value={{ user, loginUser, logoutUser }}>
             {props.children}
