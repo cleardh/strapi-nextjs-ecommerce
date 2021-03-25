883fbffd66ce3ee8db6d14aa3054b54bd841ab24
Dongha Kang <dongha@irisbooth.com>
2021-03-24 22:59:18 -0400

    Added Stripe payment solution

--- ecommerce-frontend/components/BuyButton.js
+++ ecommerce-frontend/components/BuyButton.js
@@ -1,19 +1,48 @@
 import { useContext } from 'react'
 import { Router, useRouter } from 'next/router'
+import { loadStripe } from '@stripe/stripe-js'
+
 import styles from '../styles/BuyButton.module.css'
 import AuthContext from '../context/AuthContext'
+import { STRIPE_PK, API_URL } from '../utils/urls'
 
-export default function BuyButton() {
-    const { user } = useContext(AuthContext)
+const stripePromise = loadStripe(STRIPE_PK)
+
+export default function BuyButton({ product }) {
+    const { user, getToken } = useContext(AuthContext)
     const router = useRouter()
 
     const redirectToLogin = () => {
-        Router.push('/login')
+        router.push('/login')
     }
+
+    const handleBuy = async () => {
+        const stripe = await stripePromise
+        const token = await getToken()
+
+        const res = await fetch(`${API_URL}/orders`, {
+            method: 'POST',
+            body: JSON.stringify({ product }),
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        const session = await res.json()
+
+        const result = await stripe.redirectToCheckout({
+            sessionId: session.id
+        })
+    }
+
     return (
         <>
-            {!user && (
-                <button onClick={redirectToLogin} className={styles.buy}>Login to buy</button>
+            {!user ? (
+                <button className={styles.buy} onClick={redirectToLogin}>Login to buy</button>
+            ) : (
+                <button className={styles.buy} onClick={handleBuy}>
+                    Buy
+                </button>
             )}
         </>
     )
--- ecommerce-frontend/utils/urls.js
+++ ecommerce-frontend/utils/urls.js
@@ -2,6 +2,8 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337
 
 export const MAGIC_PUBLIC_KEY = process.env.NEXT_MAGIC_PUBLIC_KEY || 'pk_test_654FF5BC0A2E8698'
 
+export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_51IYNXqDPnxnh670xX1p0YF1vAJ4j8tnoHGIFVLU0RUuCB5O4d1zqn8giGtewxT1ivO2wVGLsCfwbxDYjqcfFgZuQ00n6nxhpEd'
+
 /**
  * Given an image return the url
  * Works for local and deployed strapi
